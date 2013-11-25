4ef6ea85055acd9f22a91ec07a989a18ea5b19f4
ariyuan <wiseway@163.com>
2013-11-25 13:12:23 +0800

    Start dokan disk in program.cs

--- StarPan_New/Program.cs
+++ StarPan_New/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Security.Policy;
 using System.Text;
 using DiskAPIBase;
+using Dokan;
 
 namespace StarPan
 {
@@ -14,7 +15,7 @@ namespace StarPan
             //string root = "/apps";
             //string path = "111/22/3.txt";
             //path = PathHelper.CombineWebPath(root, path);
-            
+
             var instantce = CloudDiskManager.Instance;
             instantce.PrintFreeSpace();
 
@@ -23,6 +24,42 @@ namespace StarPan
             proxy.PrintNode(proxy.Root);
 
             Console.ReadLine();
+
+            DokanOptions opt = new DokanOptions();
+            opt.MountPoint = "r:\\";
+            opt.DebugMode = true;
+            opt.UseStdErr = true;
+            opt.VolumeLabel = "SjtuPan";
+            opt.RemovableDrive = true;
+            int status = DokanNet.DokanMain(opt, new StarPanOperations());
+
+
+
+            switch (status)
+            {
+                case DokanNet.DOKAN_DRIVE_LETTER_ERROR:
+                    Console.WriteLine("Drvie letter error");
+                    break;
+                case DokanNet.DOKAN_DRIVER_INSTALL_ERROR:
+                    Console.WriteLine("Driver install error");
+                    break;
+                case DokanNet.DOKAN_MOUNT_ERROR:
+                    Console.WriteLine("Mount error");
+                    break;
+                case DokanNet.DOKAN_START_ERROR:
+                    Console.WriteLine("Start error");
+                    break;
+                case DokanNet.DOKAN_ERROR:
+                    Console.WriteLine("Unknown error");
+                    break;
+                case DokanNet.DOKAN_SUCCESS:
+                    Console.WriteLine("Success");
+                    break;
+                default:
+                    Console.WriteLine("Unknown status: %d", status);
+                    break;
+            }
+
         }
     }
 }
